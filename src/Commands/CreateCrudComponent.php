4e7c520d169cf79c24b2efaa36838af504718972
Noaman Ahmed <noamanahmd@gmail.com>
2025-06-18 01:54:50 +0500

    Added Refresh Component Command

--- src/Commands/CreateCrudComponent.php
+++ src/Commands/CreateCrudComponent.php
@@ -83,84 +83,82 @@ class CreateCrudComponent extends Command
         $snakedCrudName = Str::snake($crudName);
 
         return [
-            [
-                'controller' => [
-                    'stub' => 'controller',
-                    'name' => "{$crudName}Controller",
-                    'path' => app_path('Http/Controllers'),
-                ],
-                'store-request' => [
-                    'stub' => 'store.request',
-                    'name' => 'StoreRequest',
-                    'path' => app_path("Http/Requests/{$crudName}"),
-                ],
-                'update-request' => [
-                    'stub' => 'update.request',
-                    'name' => 'UpdateRequest',
-                    'path' => app_path("Http/Requests/{$crudName}"),
-                ],
-                'import-request' => [
-                    'stub' => 'import.request',
-                    'name' => 'ImportRequest',
-                    'path' => app_path("Http/Requests/{$crudName}"),
-                ],
-                'export-request' => [
-                    'stub' => 'export.request',
-                    'name' => 'ExportRequest',
-                    'path' => app_path("Http/Requests/{$crudName}"),
-                ],
-                'analytics-request' => [
-                    'stub' => 'analytics.request',
-                    'name' => 'AnalyticsRequest',
-                    'path' => app_path("Http/Requests/{$crudName}"),
-                ],
-                'service' => [
-                    'stub' => 'service',
-                    'name' => "{$crudName}Service",
-                    'path' => app_path('Services'),
-                ],
-                'repository' => [
-                    'stub' => 'repository',
-                    'name' => "{$crudName}Repository",
-                    'path' => app_path('Repositories'),
-                ],
-                'transformer' => [
-                    'stub' => 'transformer',
-                    'name' => "{$crudName}Transformer",
-                    'path' => app_path('Transformers'),
-                ],
-                'collection-transformer' => [
-                    'stub' => 'transformer-collection',
-                    'name' => "{$crudName}CollectionTransformer",
-                    'path' => app_path('Transformers'),
-                ],
-                'exporter' => [
-                    'stub' => 'exporter',
-                    'name' => "{$crudName}Exporter",
-                    'path' => app_path('Exporters'),
-                ],
-                'importer' => [
-                    'stub' => 'importer',
-                    'name' => "{$crudName}Importer",
-                    'path' => app_path('Importers'),
-                ],
-                'enum' => [
-                    'stub' => 'enum',
-                    'name' => "{$crudName}StatusEnum",
-                    'path' => app_path('Enums'),
-                ],
-                'filter' => [
-                    'stub' => 'filter',
-                    'name' => "{$crudName}Filter",
-                    'path' => app_path('Filters'),
-                ],
-                'lang' => [
-                    'stub' => 'language',
-                    'name' => $snakedCrudName,
-                    'path' => base_path('resources/lang/en'),
-                    'extension' => '.php',
-                ],
-            ]
+            'controller' => [
+                'stub' => 'controller',
+                'name' => "{$crudName}Controller",
+                'path' => app_path('Http/Controllers'),
+            ],
+            'store-request' => [
+                'stub' => 'store.request',
+                'name' => 'StoreRequest',
+                'path' => app_path("Http/Requests/{$crudName}"),
+            ],
+            'update-request' => [
+                'stub' => 'update.request',
+                'name' => 'UpdateRequest',
+                'path' => app_path("Http/Requests/{$crudName}"),
+            ],
+            'import-request' => [
+                'stub' => 'import.request',
+                'name' => 'ImportRequest',
+                'path' => app_path("Http/Requests/{$crudName}"),
+            ],
+            'export-request' => [
+                'stub' => 'export.request',
+                'name' => 'ExportRequest',
+                'path' => app_path("Http/Requests/{$crudName}"),
+            ],
+            'analytics-request' => [
+                'stub' => 'analytics.request',
+                'name' => 'AnalyticsRequest',
+                'path' => app_path("Http/Requests/{$crudName}"),
+            ],
+            'service' => [
+                'stub' => 'service',
+                'name' => "{$crudName}Service",
+                'path' => app_path('Services'),
+            ],
+            'repository' => [
+                'stub' => 'repository',
+                'name' => "{$crudName}Repository",
+                'path' => app_path('Repositories'),
+            ],
+            'transformer' => [
+                'stub' => 'transformer',
+                'name' => "{$crudName}Transformer",
+                'path' => app_path('Transformers'),
+            ],
+            'collection-transformer' => [
+                'stub' => 'transformer-collection',
+                'name' => "{$crudName}CollectionTransformer",
+                'path' => app_path('Transformers'),
+            ],
+            'exporter' => [
+                'stub' => 'exporter',
+                'name' => "{$crudName}Exporter",
+                'path' => app_path('Exporters'),
+            ],
+            'importer' => [
+                'stub' => 'importer',
+                'name' => "{$crudName}Importer",
+                'path' => app_path('Importers'),
+            ],
+            'enum' => [
+                'stub' => 'enum',
+                'name' => "{$crudName}StatusEnum",
+                'path' => app_path('Enums'),
+            ],
+            'filter' => [
+                'stub' => 'filter',
+                'name' => "{$crudName}Filter",
+                'path' => app_path('Filters'),
+            ],
+            'lang' => [
+                'stub' => 'language',
+                'name' => $snakedCrudName,
+                'path' => base_path('resources/lang/en'),
+                'extension' => '.php',
+            ],
         ];
     }
 }
